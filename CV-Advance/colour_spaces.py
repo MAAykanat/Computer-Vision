abe6bbd28bca9e9d7fb79c9d49b76f84d4bf4256
MAAykanat <m.a.aykanat@gmail.com>
2022-08-04 17:27:51 +0300

    Advance Section file is created

---
+++ CV-Advance/colour_spaces.py
@@ -0,0 +1,15 @@
+### Used image, video and gif dataset is taken from Kaggle and link is below
+### |--------------------------------------------------------------------------------| ###
+###https://www.kaggle.com/datasets/bulentsiyah/opencv-samples-images?resource=download
+### |--------------------------------------------------------------------------------| ###
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        pass
