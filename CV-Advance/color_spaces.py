262cdae5000642d06e146c847273b022e3cb8308
MAAykanat <m.a.aykanat@gmail.com>
2022-08-05 08:59:20 +0300

    ColorSpace work is added

--- CV-Advance/color_spaces.py
+++ CV-Advance/color_spaces.py
@@ -3,11 +3,48 @@
 ### https://www.kaggle.com/datasets/bulentsiyah/opencv-samples-images?resource=download
 ### |--------------------------------------------------------------------------------| ###
 
+    ###NOTES###
+    #------------1-------------#
+    # OpenCv uses BGR as default and knows opened image as same as shown in folder
+    # On the other hand matplotlib uses RGB thats why they show different.
+    # Try= plt.imshow(img) \n plt.show()
+    #------------2-------------#
+    #
+    ###SOURCES###
+    #---------TOPIC---------#
+
+    #-----------------------------------#
+
 
 import cv2 as cv
-
+import matplotlib.pyplot as plt
 def main():
-    pass
+    path = "/home/maaykanat/Desktop/OwnStudy/Usable Data/data/"
+
+    img = cv.imread(path + "starry_night.jpg")
+    cv.imshow("Original Image", img)
+
+    # plt.imshow(img)
+    # plt.show() 
+
+    #BGR to GrayScale
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    cv.imshow("Gray", gray)
+
+    #BGR to HSV
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    cv.imshow("HSV", hsv)
+
+    #BGR to L*a*b
+    lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+    cv.imshow("L*a*b", lab)
+
+    # BGR to RGB
+    rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    cv.imshow("RGB", rgb)
+
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
 if __name__ == '__main__':
     try:
