34d494251691ed5b1e9d5f3ee69bf9a9bc91913b
MAAykanat <m.a.aykanat@gmail.com>
2022-08-08 08:54:31 +0300

    bitwise.py is added

---
+++ CV-Advance/bitwise.py
@@ -0,0 +1,19 @@
+### Used image, video and gif dataset is taken from Kaggle and link is below
+### |--------------------------------------------------------------------------------| ###
+### https://www.kaggle.com/datasets/bulentsiyah/opencv-samples-images?resource=download
+### |--------------------------------------------------------------------------------| ###
+
+    ###NOTES###
+    
+    ###SOURCES###
+   
+import cv2 as cv
+
+def main():
+    path = ""
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        pass
