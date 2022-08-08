59034f3f15601c0f8d4ebc63f9d8d887dc8cd640
MAAykanat <m.a.aykanat@gmail.com>
2022-08-08 09:31:27 +0300

    AND-OR-XOR are added to bitwise.py

--- CV-Advance/bitwise.py
+++ CV-Advance/bitwise.py
@@ -4,13 +4,37 @@
 ### |--------------------------------------------------------------------------------| ###
 
     ###NOTES###
-    
+    # Bitwise operation widely use in masking    
     ###SOURCES###
-   
+    
 import cv2 as cv
+import numpy as np
 
 def main():
-    path = ""
+
+    blank = np.zeros((400,400), dtype="uint8")
+    cv.imshow("Blank-Image", blank)
+
+    rectangle = cv.rectangle(blank.copy(), (30,30), (250,250), color=255, thickness=-1)
+    cv.imshow("Rectangle", rectangle)
+
+    circle = cv.circle(blank.copy(), (200,200), radius=125, color=255, thickness=-1)
+    cv.imshow("Circle", circle)
+
+    #AND Opetaration --> Intersecting Areas
+    bitwise_and = cv.bitwise_and(rectangle, circle)
+    cv.imshow("Bw-AND", bitwise_and)
+
+    #OR Operation --> Non-Intersecting and Intersecting Ares
+    bitwise_or = cv.bitwise_or(rectangle, circle)
+    cv.imshow("Bw-OR", bitwise_or)
+
+    #XOR Opetation --> Non-Intersecting Areas
+    bitwise_xor = cv.bitwise_xor(rectangle, circle)
+    cv.imshow("Bw-XOR", bitwise_xor)
+
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
 if __name__ == '__main__':
     try:
