751ae4b7c864465fb2d4b730e4cae565927514e0
MAAykanat <m.a.aykanat@gmail.com>
2022-08-10 09:37:22 +0300

    Laplacian-Sobel-Canny is added

--- CV-Advance/gradients.py
+++ CV-Advance/gradients.py
@@ -1,8 +1,36 @@
 
 import cv2 as cv
+import numpy as np
 
 def main():
-    pass
+    path ="D:/!!!MAAykanat Dosyalar/MAA_Own_Study/Usable Data"
+    
+    img = cv.imread(path + "/data/building.jpg")
+    cv.imshow("Original-Image", img)
+
+    img = cv.resize(img, (int(img.shape[1]*0.75), int(img.shape[0]*0.75)))
+
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    cv.imshow("Gray", gray)
+
+    #Laplacian
+    lap = cv.Laplacian(gray, cv.CV_64F)
+    lap = np.uint8(np.absolute(lap))
+    cv.imshow("Laplacian",lap)
+
+    #Sobel
+    sobelx =cv.Sobel(gray, cv.CV_64F, 1, 0)
+    sobely =cv.Sobel(gray, cv.CV_64F, 0, 1)
+    combined_sobel = cv.bitwise_or(sobelx, sobely)
+    cv.imshow("Combined Sobel", combined_sobel)
+
+    #Canny
+    canny= cv.Canny(gray, 200, 220)
+    cv.imshow("Canny", canny)
+
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
 
 if __name__=='__main__':
     try:
