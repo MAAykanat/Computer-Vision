abe03dc3beb61c69ebb4e9a25165fb951ee19f52
Muhammet Ali Aykanat <muhammet.aykanat@denermakina.local>
2022-08-10 08:03:57 +0300

    Blur is added to thresh to compare

--- CV-Advance/thresh.py
+++ CV-Advance/thresh.py
@@ -17,14 +17,24 @@ def main():
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     cv.imshow("Gray-Image", gray)
 
+    blur = cv.GaussianBlur(img, ksize=(5,5),sigmaX=5,sigmaY=0, borderType=cv.BORDER_DEFAULT)
+    cv.imshow("Blur-Image", blur)
+
+    blur_gray = cv.cvtColor(blur, cv.COLOR_BGR2GRAY)
     # Simple Tresholding
     threshold, thresh = cv.threshold(gray, thresh=125 , maxval= 255, type=cv.THRESH_BINARY_INV)
     cv.imshow("Thresholding-Image", thresh)
     print("Threshold value is: ", threshold)
 
+    threshold_blur, thresh_blur = cv.threshold(blur_gray, thresh=125 , maxval= 255, type=cv.THRESH_BINARY_INV)
+    cv.imshow("Blur Thresholding-Image", thresh_blur)
+
     # Adaptive Thresholding
     adaptive_thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY_INV, 3, 7)
     cv.imshow("Adaptive-Treshold", adaptive_thresh)
+
+    adaptive_thresh_blur = cv.adaptiveThreshold(blur_gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY_INV, 7, 7)
+    cv.imshow("Blur Adaptive-Treshold", adaptive_thresh_blur)
 
     cv.waitKey(0)
     cv.destroyAllWindows()
