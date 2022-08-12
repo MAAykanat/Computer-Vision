d3d01ef034f65453a0be5825e66affec4635508f
MAAykanat <m.a.aykanat@gmail.com>
2022-08-12 09:15:24 +0300

    face_rectangle function is added

--- Face-Detection/face_detect.py
+++ Face-Detection/face_detect.py
@@ -1,5 +1,15 @@
 
+
 import cv2 as cv
+
+def face_rectangle(img, gray, scaleFactor, minNeigbors):
+    haar_cascade = cv.CascadeClassifier('.\Face-Detection\haar_face.xml')
+    faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=scaleFactor, minNeighbors=minNeigbors)
+
+    print("Number of faces found: ", len(faces_rect))
+    for (x,y,w,h) in faces_rect:
+        cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=3)
+    return img
 
 def main():
         
@@ -14,14 +24,8 @@ def main():
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     cv.imshow('Gray Image', gray)
 
-    haar_cascade = cv.CascadeClassifier('.\Face-Detection\haar_face.xml')
+    face_rectangle(img,gray, scaleFactor= 1.3, minNeigbors=5)
 
-    faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
-    print("Number of faces found: ", len(faces_rect))
-
-    for (x,y,w,h) in faces_rect:
-        cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=2)
-    
     cv.imshow('Detected Faces', img)
     cv.waitKey()
     cv.destroyAllWindows()
@@ -33,11 +37,7 @@ def main():
     gray_group1 = cv.cvtColor(group1, cv.COLOR_BGR2GRAY)
     cv.imshow("Gray-Group1", gray_group1)
 
-    faces_rect = haar_cascade.detectMultiScale(gray_group1, scaleFactor=1.1, minNeighbors=3)
-    print("Number of faces found in group: ", len(faces_rect))
-
-    for (x,y,w,h) in faces_rect:
-        cv.rectangle(group1, (x,y), (x+w,y+h), (255,0,0), thickness=2)
+    face_rectangle(group1, gray_group1, scaleFactor= 1.1, minNeigbors=3)
     
     cv.imshow("Group1-Detected Image",group1)
     cv.waitKey(0)
@@ -50,11 +50,7 @@ def main():
     gray_group2 = cv.cvtColor(group2, cv.COLOR_BGR2GRAY)
     cv.imshow("Gray-Group1", gray_group2)
 
-    faces_rect = haar_cascade.detectMultiScale(gray_group2, scaleFactor=1.3, minNeighbors=5)
-    print("Number of faces found in group: ", len(faces_rect))
-
-    for (x,y,w,h) in faces_rect:
-        cv.rectangle(group2, (x,y), (x+w,y+h), (0,255,0), thickness=2)
+    face_rectangle(group2, gray_group2, scaleFactor= 1.3, minNeigbors=5)
     
     cv.imshow("Group2-Detected Image", group2)
     cv.waitKey(0)
