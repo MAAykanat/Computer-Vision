47bc4a4a36f391f02bc208a7f4ae387ed7f8149e
MAAykanat <m.a.aykanat@gmail.com>
2022-08-12 08:51:21 +0300

    Group Photos added

--- Face-Detection/face_detect.py
+++ Face-Detection/face_detect.py
@@ -1,24 +1,67 @@
+
 import cv2 as cv
 
-path = "D:/!!!MAAykanat Dosyalar/MAA_Own_Study/CV-Data/Face-Detection"
+def main():
+        
+    path = "D:/!!!MAAykanat Dosyalar/MAA_Own_Study/CV-Data/Face-Detection"
 
-img = cv.imread(path + '/foto.png')
-cv.imshow('Original Image', img)
+    img = cv.imread(path + '/foto.png')
+    cv.imshow('Original Image', img)
+    print(img.shape) #--> Image shape is (1116, 1060, 3)
 
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('Gray Image', gray)
+    img = cv.resize(img,((int(img.shape[1]*0.75)), int(img.shape[0]*0.75)))
 
-haar_cascade = cv.CascadeClassifier('D:\!!!MAAykanat Dosyalar\MAA_Own_Study\Computer Vision\Face-Detection\haar_face.xml')
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    cv.imshow('Gray Image', gray)
 
-faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1)
+    haar_cascade = cv.CascadeClassifier('.\Face-Detection\haar_face.xml')
 
-print(f'Number of faces found = {len(faces_rect)}')
+    faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
+    print("Number of faces found: ", len(faces_rect))
 
-for (x,y,w,h) in faces_rect:
-    cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=2)
+    for (x,y,w,h) in faces_rect:
+        cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=2)
+    
+    cv.imshow('Detected Faces', img)
+    cv.waitKey()
+    cv.destroyAllWindows()
+    
+    # Group-1 Photo
+    group1 = cv.imread(path + '/Group1.jpeg')
+    cv.imshow("Group-1 Photo", group1)
 
-cv.imshow('Detected Faces', img)
+    gray_group1 = cv.cvtColor(group1, cv.COLOR_BGR2GRAY)
+    cv.imshow("Gray-Group1", gray_group1)
 
+    faces_rect = haar_cascade.detectMultiScale(gray_group1, scaleFactor=1.1, minNeighbors=3)
+    print("Number of faces found in group: ", len(faces_rect))
 
+    for (x,y,w,h) in faces_rect:
+        cv.rectangle(group1, (x,y), (x+w,y+h), (255,0,0), thickness=2)
+    
+    cv.imshow("Group1-Detected Image",group1)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
-cv.waitKey(0)
+    # Group-2 Photo
+    group2 = cv.imread(path + '/Group2.jpeg')
+    cv.imshow("Group-1 Photo", group2)
+
+    gray_group2 = cv.cvtColor(group2, cv.COLOR_BGR2GRAY)
+    cv.imshow("Gray-Group1", gray_group2)
+
+    faces_rect = haar_cascade.detectMultiScale(gray_group2, scaleFactor=1.3, minNeighbors=5)
+    print("Number of faces found in group: ", len(faces_rect))
+
+    for (x,y,w,h) in faces_rect:
+        cv.rectangle(group2, (x,y), (x+w,y+h), (0,255,0), thickness=2)
+    
+    cv.imshow("Group2-Detected Image", group2)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        pass
